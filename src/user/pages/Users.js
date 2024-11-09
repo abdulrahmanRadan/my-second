bc3ea547d5ce7eef68849e89e63c907ea6d0fdf1
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-09 03:19:21 +0300

    Adding a UsersList Page Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,7 +3,16 @@ import React from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <UsersList />;
+  const USERS = [
+    {
+      id: "u1",
+      name: "abdulrahman",
+      image:
+        "https://www.shutterstock.com/image-photo/woman-using-chatbot-computer-tablet-260nw-2276323975.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
