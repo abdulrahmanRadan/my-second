fa5e53b807c1a907797ef0b5c816b0bb799e0fec
abdulrahmanRadan <abdulrahmanRadan@users.noreply.github.com>
2024-11-09 13:45:48 +0000

    deploy: 9b41ac6d57a23b76d71d82403e541502a1ed87ab

---
+++ precache-manifest.b51da96dbb52cac856d39fbe03281091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76a992b33c31a7c315cf7adcb00ac16",
+    "url": "/my-second/index.html"
+  },
+  {
+    "revision": "6fa441d465bb9c58abeb",
+    "url": "/my-second/static/css/main.10812776.chunk.css"
+  },
+  {
+    "revision": "12e00b675e5a97d12641",
+    "url": "/my-second/static/js/2.29c4125b.chunk.js"
+  },
+  {
+    "revision": "fac1f3ff5e8ebdfed3580bcec7c38cc3",
+    "url": "/my-second/static/js/2.29c4125b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fc03857434fc43e671",
+    "url": "/my-second/static/js/3.ef9bfbd4.chunk.js"
+  },
+  {
+    "revision": "6fa441d465bb9c58abeb",
+    "url": "/my-second/static/js/main.f712d020.chunk.js"
+  },
+  {
+    "revision": "bb92895f7d803dcc753f",
+    "url": "/my-second/static/js/runtime-main.b887109c.js"
+  }
+]);
