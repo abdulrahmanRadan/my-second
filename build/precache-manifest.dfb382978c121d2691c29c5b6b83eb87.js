9ca15fce1f1fdff056ae08fee83cb3f0e47a5580
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-09 14:46:49 +0300

    run build

---
+++ build/precache-manifest.dfb382978c121d2691c29c5b6b83eb87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f1c40dc0a44d55e09efdcb9e2ffaea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df36802e8421a2e0a22f",
+    "url": "/static/css/main.b610d3d6.chunk.css"
+  },
+  {
+    "revision": "fdba0ed642f13ab077f7",
+    "url": "/static/js/2.68f98e0d.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "df36802e8421a2e0a22f",
+    "url": "/static/js/main.6dfda422.chunk.js"
+  },
+  {
+    "revision": "38b441cba92b9c74f03f",
+    "url": "/static/js/runtime~main.16e34d5d.js"
+  }
+]);
