cd9d2699c7e3d639831cd048a4b0fc543d5cf69c
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-11 16:43:01 +0300

    Adding a Main Header

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 function App() {
   return (
     <BrowserRouter
       future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
     >
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/place/newplace" element={<NewPlace />} />
-        {/* Redirect to home page */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/place/newplace" element={<NewPlace />} />
+          {/* Redirect to home page */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
