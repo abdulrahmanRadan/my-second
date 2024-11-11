de4582f2376cdaffb807890812b42db2e295048c
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-11 18:08:20 +0300

    Add NavLinks and Implementing a Basic Desktop & Mobile Navigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import MainHeader from "./MainHeader.js";
 import "./MainNavigation.css";
+import MainHeader from "./MainHeader.js";
+import NavLinks from "./NavLink.js";
+import SideDrawer from "./SideDrawer.js";
 
 const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlace</Link>
-      </h1>
-      <nav>...</nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-var">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlace</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
