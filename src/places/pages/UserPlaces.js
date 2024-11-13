edbcb0d90fd47e618379bcde18665d80b34e07c0
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-13 18:15:04 +0300

    Adding a Custom Button

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
@@ -13,7 +14,7 @@ const DUMMY_PLACES = [
       lat: 15.4237743,
       lng: 44.1906041,
     },
-    creator: "abod",
+    creator: "u1",
   },
   {
     id: "p2",
@@ -26,12 +27,14 @@ const DUMMY_PLACES = [
       lat: 15.4237743,
       lng: 44.1906041,
     },
-    creator: "abod",
+    creator: "u2",
   },
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
