b99d55ef3e4598644a6d2a9cdd22a55bb6315773
abdulrahmanRadan <aboda123li123@gmail.com>
2024-11-14 20:58:11 +0300

    Rendering a Map with Google Maps without a js sdk

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect } from "react";
+import "ol/ol.css";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import { fromLonLat } from "ol/proj";
+
+import "./Map.css";
+
+const MapComponent = (props) => {
+  const mapRef = useRef();
+
+  const { center, zoom } = props;
+
+  useEffect(() => {
+    new Map({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([center.lng, center.lat]),
+        zoom: zoom,
+      }),
+    });
+  }, [center, zoom]);
+
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
+};
+
+export default MapComponent;
